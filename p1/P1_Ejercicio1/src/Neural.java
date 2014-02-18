c3590e8e51458cbd57c778c747f3dbf24329a08e
OlympiaSH <omuruzabal@gmail.com>
2014-02-18 19:54:52 +0100

    0218 comentarios en Neural.java

--- p1/P1_Ejercicio1/src/Neural.java
+++ p1/P1_Ejercicio1/src/Neural.java
@@ -16,30 +16,32 @@ public class Neural {
 	 * @return
 	 */
 	public ArrayList<Neuron> calculateValues(ArrayList<Integer> data){
+		/*Calcula el valor de cada neurona*/
 		for(int i=0; i<neural.size(); i++){
 			double sum = 0.0;
 			Iterator it = neural.get(i).getSignals().entrySet().iterator();		
 			while(it.hasNext()){
 				Entry<Neuron,Double> e = (Entry<Neuron,Double>) it.next();
-				sum += (e.getKey().getValue() * e.getValue());
+				sum += (e.getKey().getValue() * e.getValue()); //Calcula el sumatorio de senales de entrada
 			}
+			/*Actualiza el valor de la neurona segun el umbral*/
 			if(sum>=neural.get(i).getThreadhold()){
 				neural.get(i).setValue(1);
 			}else{
 				neural.get(i).setValue(0);
 			}
-		}
-		
+		}		
 		return neural;
 	}
 	
 	public void createNeural(ArrayList<Integer> data){
 		layer = new ArrayList<ArrayList<Neuron>>();
 		layer.add(new ArrayList<Neuron>());
+		/*Actualiza la capa de entrada*/
 		for(int i=0; i< data.size(); i++){
 			layer.get(0).add(new Neuron(0,data.get(i)));
 		}
-		
+		/*Crea la red neuronal*/
 	}
 	
 	public ArrayList<ArrayList<Double>> getWeighs() {
