087c79078564f5d3c038ae69ef0b66140f7c9256
mqgmaster <mqgmaster@gmail.com>
2014-02-24 18:55:50 +0100

    ejecutado problema_real1.txt y a primera vista el perceptron está perfecto.

--- p1/perceptron/src/es/uam/eps/neuro/perceptron/Main.java
+++ p1/perceptron/src/es/uam/eps/neuro/perceptron/Main.java
@@ -6,7 +6,7 @@ import es.uam.eps.neuro.perceptron.service.FileService;
 public class Main {
 	
 	public static void main(String[] args) {
-		InputData data = new InputData(FileService.read("../test.txt"));
+		InputData data = new InputData(FileService.read("../problema_real1.txt"));
 		Perceptron perceptron = new Perceptron(data);
 		perceptron.startTraining();
 		perceptron.startTest();
